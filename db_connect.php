009b3cb6095313f5d6b3a958e261cc7c7b1c6749
loonbox <studio@douglastitus.com>
2020-01-19 08:14:09 -0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "phpwork";
+$username = "xxx";
+$password = "xxxx";
 $dbname = "cms";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
